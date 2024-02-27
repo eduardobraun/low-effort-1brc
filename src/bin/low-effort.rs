177f40c8dfc681d45af6cf4366401f98cc2b68d4
Eduardo Braun <eduardotbraun@gmail.com>
2024-02-27 12:46:59 -0300

    add hacky i16 float parsing

--- src/bin/low-effort.rs
+++ src/bin/low-effort.rs
@@ -8,39 +8,61 @@ use rayon::prelude::*;
 
 #[derive(Debug, Clone)]
 struct Statistics {
-    min: f64,
-    max: f64,
-    sum: f64,
+    min: i16,
+    max: i16,
+    sum: i64,
     count: usize,
 }
 
 impl Statistics {
-    fn new(measurement: f64) -> Self {
+    #[inline]
+    fn new(measurement: i16) -> Self {
         Statistics {
             min: measurement,
             max: measurement,
-            sum: measurement,
+            sum: measurement as i64,
             count: 1,
         }
     }
 
+    #[inline]
     fn average(&self) -> f64 {
-        self.sum / self.count as f64
+        (self.sum / self.count as i64) as f64 / 10.
     }
 
-    fn add_measurement(&mut self, measurement: f64) {
+    #[inline]
+    fn add_measurement(&mut self, measurement: i16) {
         self.count += 1;
         self.min = measurement.min(self.min);
         self.max = measurement.max(self.max);
-        self.sum += measurement;
+        self.sum += measurement as i64;
     }
 
+    #[inline]
     fn merge(&mut self, other: &Statistics) {
         self.count += other.count;
         self.min = other.min.min(self.min);
         self.max = other.max.max(self.max);
         self.sum += other.sum;
     }
+}
+
+#[inline]
+fn hacky_i16_float_parse(buf: &[u8]) -> i16 {
+    let (sign, buf) = if buf[0] == b'-' {
+        (-1i16, &buf[1..])
+    } else {
+        (1i16, buf)
+    };
+
+    let num: i16 = buf
+        .iter()
+        .filter(|b| b.is_ascii_digit())
+        .fold(0i16, |acc, b| {
+            let decimal = (b - b'0') as i16;
+            acc * 10 + decimal
+        });
+    num * sign
 }
 
 fn main() -> anyhow::Result<()> {
@@ -52,10 +74,7 @@ fn main() -> anyhow::Result<()> {
         .fold_with(HashMap::<&[u8], Statistics>::new(), |mut map, buf| {
             let pos = buf.iter().position(|b| b == &b';').unwrap();
             let (station, temperature_u8) = buf.split_at(pos);
-            let temperature = std::str::from_utf8(&temperature_u8[1..])
-                .unwrap()
-                .parse::<f64>()
-                .unwrap();
+            let temperature = hacky_i16_float_parse(&temperature_u8[1..]);
 
             if let Some(stored_stats) = map.get_mut(station) {
                 stored_stats.add_measurement(temperature);
@@ -97,16 +116,26 @@ fn main() -> anyhow::Result<()> {
 
     for (station, stats) in result {
         println!(
-            "{:?}, count: {}, min: {:.2}, max: {:.2}, avg: {:.2}",
+            "{}, count: {}, min: {:.1}, max: {:.1}, avg: {:.1}",
             std::str::from_utf8(station)?,
-            // std::borrow::Cow::Borrowed(station),
-            // String::from_utf8_lossy(station),
             stats.count,
-            stats.min,
-            stats.max,
+            stats.min as f64 / 10.0,
+            stats.max as f64 / 10.0,
             stats.average(),
         );
     }
 
     Ok(())
+}
+
+#[test]
+fn parse_positive() {
+    assert_eq!(hacky_i16_float_parse("12.0".as_bytes()), 120);
+    assert_eq!(hacky_i16_float_parse("99.9".as_bytes()), 999);
+}
+
+#[test]
+fn parse_negative() {
+    assert_eq!(hacky_i16_float_parse("-12.0".as_bytes()), -120);
+    assert_eq!(hacky_i16_float_parse("-99.9".as_bytes()), -999);
 }
